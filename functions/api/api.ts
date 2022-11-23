06cb11b892dc72d22d8a1606e966b8e86b1d6b20
Sil van Diepen <me@sil.mt>
2022-11-23 16:23:34 +0100

    add colors

--- functions/api/api.ts
+++ functions/api/api.ts
@@ -1,16 +1,22 @@
-import { Handler } from '@netlify/functions'
+import { Handler } from "@netlify/functions";
+import { toHex, toCMYK, toRGB, toHSL } from "@sil/color";
 
-export const handler:Handler = async (event, context) => {
-  let colorName = 'stranger';
-  if(event.queryStringParameters){
-    const { name = 'stranger' } = event.queryStringParameters
-    colorName = name;
+export const handler: Handler = async (event, context) => {
+  let input = "#cccccc";
+  if (event.queryStringParameters) {
+    const { color = input } = event.queryStringParameters;
+    input = color;
   }
+
+  const output = {
+    hex: toHex(input),
+    cmyk: toCMYK(input),
+    rgb: toRGB(input),
+    hsl: toHSL(input),
+  };
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${colorName}!`,
-    }),
-  }
-}
+    body: JSON.stringify(output),
+  };
+};
